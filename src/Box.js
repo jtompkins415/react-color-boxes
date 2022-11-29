dcf44615205884dcba1f8b595e9cd91466df4961
jtompkins415 <john.tompkins415@gmail.com>
2022-11-28 21:19:35 -0800

    add defaults to Box props, add name attribute, change box size units

--- src/Box.js
+++ src/Box.js
@@ -1,16 +1,16 @@
 import React from "react";
 import "./Box.css"
 
-const Box = ({id, color, width, height, removeBox }) => {
+const Box = ({id, color = 'black', width = 5, height = 5, removeBox }) => {
 
     const remove = () => removeBox(id)
 
     return (
         <div className="Box">
-            <div className="Box-box" style={{
+            <div className="Box-box" name='box' style={{
             backgroundColor: `${color}`,
-            width: `${width}px`,
-            height: `${height}px`,
+            width: `${width}em`,
+            height: `${height}em`,
             display: "inline-block"}}>
             </div>
             <button onClick={remove}><b>X</b></button>
